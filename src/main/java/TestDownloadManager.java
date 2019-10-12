1232a7c2b9efd94ad16abc9c83ce94b32b804fab
ElSindarin <53188567+ElSindarin@users.noreply.github.com>
2019-10-13 01:54:13 +0300

    Added an opportunity to avoid speed input

--- src/main/java/TestDownloadManager.java
+++ src/main/java/TestDownloadManager.java
@@ -13,7 +13,10 @@ public class TestDownloadManager {
         }
         String src = console.readLine("Enter source file path");
         String dest = console.readLine("Enter destination file path");
-        String speedInChars = console.readLine("Enter speed of copying, in chars (approximately value of 8 or so is appropriate for my file)");
+        String speedInChars = console.readLine("Enter speed of copying, in chars (approximately value of 8 or so is appropriate for my file). If you do not want to input speed, type 0");
+        if (Integer.valueOf(speedInChars) == 0) {
+            speedInChars = "8";
+        }
         String duration = console.readLine("Enter time in milliseconds as an upper boundary of this program operation (recommended value for my file is 2-3)");
         download(src,dest,Integer.valueOf(speedInChars),Integer.valueOf(duration));
     }
